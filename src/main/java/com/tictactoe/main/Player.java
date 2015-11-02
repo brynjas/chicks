8855b31b9a7138923f5e8834f08827aac5b29361
brynjas <brynja13@ru.is>
2015-11-02 21:27:43 +0000

    b

---
+++ src/main/java/com/tictactoe/main/Player.java
@@ -0,0 +1,17 @@
+package com.tictactoe.main;
+
+public class Player{
+
+    private char symbol;
+   
+    public Player (char s){
+	symbol = s;
+    }
+    
+    public char getSymbol(){
+	return symbol;
+    }
+   
+}
+
+
